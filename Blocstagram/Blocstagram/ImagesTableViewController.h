1420c8d7a0754ed9c649d0e1f4fd7f624a463bc6
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-04-15 13:42:31 -0400

    Checkpoint 27: images not displaying

---
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Joanna Lingenfelter on 4/15/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
