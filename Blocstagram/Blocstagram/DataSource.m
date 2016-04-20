ebc92971767da48a2a0401ee9cd06ad4ebce62d0
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-04-20 10:39:08 -0400

    Checkpoint 32: complete

--- Blocstagram/Blocstagram/DataSource.m
+++ Blocstagram/Blocstagram/DataSource.m
@@ -18,6 +18,9 @@
 
 @property (nonatomic, strong) NSArray *mediaItems;
 
+@property (nonatomic, assign) BOOL isRefreshing;
+@property (nonatomic, assign) BOOL isLoadingOlderItems;
+
 @end
 
 
@@ -164,9 +167,52 @@
     
 }
 
+#pragma mark - Completion Handler
 
+- (void) requestNewItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler {
+    
+    // #1
+    if (self.isRefreshing == NO) {
+        self.isRefreshing = YES;
+    }
+    
+    // #2
+    Media *media = [[Media alloc] init];
+    media.user = [self randomUser];
+    media.image = [UIImage imageNamed:@"10.jpg"];
+    media.caption = [self randomSentence];
+    
+    NSMutableArray *mutableArrayWIthKVO = [self mutableArrayValueForKey:@"mediaItems"];
+    [mutableArrayWIthKVO insertObject:media atIndex:0];
+    
+    self.isRefreshing = NO;
+    
+    if (completionHandler) {
+        completionHandler(nil);
+    }
+    
+}
 
-
+- (void) requestOldItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler {
+    
+    if (self.isLoadingOlderItems == NO) {
+        
+        self.isLoadingOlderItems = YES;
+        Media *media = [[Media alloc] init];
+        media.user = [self randomUser];
+        media.image = [UIImage imageNamed:@"1.jpg"];
+        media.caption = [self randomSentence];
+        
+        NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
+        [mutableArrayWithKVO addObject:media];
+        
+        self.isLoadingOlderItems = NO;
+        
+        if (completionHandler) {
+            completionHandler(nil);
+        }
+    }
+}
 
 
 
