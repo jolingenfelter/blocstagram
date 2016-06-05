0fd2cd1f1b12bb5e024733b45059012154a6c656
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-06-05 13:19:44 -0400

    Checkpoint 41: CameraToolbar constraint issue

---
+++ Blocstagram/Blocstagram/CameraToolbar.h
@@ -0,0 +1,27 @@
+//
+//  CameraToolbar.h
+//  Blocstagram
+//
+//  Created by Joanna Lingenfelter on 6/1/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CameraToolbar;
+
+@protocol CameraToolbarDelegate <NSObject>
+
+- (void) leftButtonPressedOnToolbar:(CameraToolbar *)toolbar;
+- (void) rightButtonPressedOnToolbar:(CameraToolbar *)toolbar;
+- (void) cameraButtonPressedOnToolbar:(CameraToolbar *)toolbar;
+
+@end
+
+@interface CameraToolbar : UIView
+
+- (instancetype) initWithImageNames:(NSArray *)imageNames;
+
+@property (nonatomic, weak) NSObject <CameraToolbarDelegate> *delegate;
+
+@end
