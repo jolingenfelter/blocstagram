fa0f30846b6a592b05b74c53eeacd97133cd1e11
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2016-06-13 17:53:02 -0400

    Assignment 45: mediaItem height test troubles

--- Blocstagram/BlocstagramTests/MediaTableViewCellTests.m
+++ Blocstagram/BlocstagramTests/MediaTableViewCellTests.m
@@ -12,6 +12,8 @@
 
 @interface MediaTableViewCellTests : XCTestCase
 
+@property (nonatomic, strong) NSDictionary *sourceDictionary;
+
 @end
 
 @implementation MediaTableViewCellTests
@@ -19,6 +21,16 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.sourceDictionary= @{@"id": @"9999999",
+                        @"images" : @{@"standard_resolution" : @{@"url" : @"http://www.melbo.co"}},
+                        @"user_had_liked" : @"0",
+                        @"caption" : @{@"text" : @"texttesxttext"},
+                        @"user" : @{@"id" : @"1234567",
+                                    @"username" : @"Freckles",
+                                    @"full_name" : @"Homer Simpson",
+                                    @"profile_picture" : @"@http://www.example.com/example.jpg"}
+                        };
 }
 
 - (void)tearDown {
@@ -26,13 +38,33 @@
     [super tearDown];
 }
 
-- (void) testAccurateHeightOfMediaItem {
+- (void) testHeightOfMediaItem {
     
-    MediaTableViewCell *testCell = [[MediaTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+
+    MediaTableViewCell *testCell1 = [[MediaTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    MediaTableViewCell *testCell2 = [[MediaTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
     
-    NSDictionary *testImages = @{@"image" : [UIImage imageNamed: @"4.jpg"]};
+    UIImage *testImage1 = [UIImage imageNamed: @"4.jpg"];
+    UIImage *testImage2 = [UIImage imageNamed:@"10.jpg"];
     
-    Media *testMedia = [[Media alloc] initWithDictionary:testImages];
+    Media *testMedia1 = [[Media alloc] initWithDictionary:self.sourceDictionary];
+    testMedia1.image = testImage1;
+    Media *testMedia2 = [[Media alloc] initWithDictionary:self.sourceDictionary];
+    testMedia2.image = testImage2;
+    
+    [testCell1 setMediaItem:testMedia1];
+    [testCell2 setMediaItem:testMedia2];
+    
+    CGFloat width = 423;
+    CGFloat imageHeight1 = [MediaTableViewCell heightForMediaItem:testMedia1 width:width traitCollection:testCell1.traitCollection];
+    CGFloat imageHeight2 = [MediaTableViewCell heightForMediaItem:testMedia2 width:width traitCollection:testCell2.traitCollection];
+    
+    XCTAssertGreaterThan(imageHeight1, 225);
+    XCTAssertGreaterThan(imageHeight2, 225);
+    
+    XCTAssertLessThan(imageHeight1, 250);
+    XCTAssertLessThan(imageHeight2, 250);
+
 
 }
 @end
